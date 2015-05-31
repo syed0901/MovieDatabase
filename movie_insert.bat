05cfc2602dc41f27c76734b6a24c64cfd7d2d038
syed <swmr88@gmail.com>
2015-05-31 13:45:37 +0530

    Create movie_insert.bat
    This batch file picks the movie_inser.sql created by python script and insert in mySQL db

---
+++ movie_insert.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+SET MYSQL_EXE="C:\xampp\mysql\bin\mysql.exe"
+SET DB_USER=ROOT here
+SET DB_PWD=PASSWORD here
+SET DB_DATABASE=DB name here
+
+CALL %MYSQL_EXE% --user=%DB_USER% --password=%DB_PWD% --database=%DB_DATABASE% < C:\Users\srizvi\Projects\MovieDatabase\movie_insert.sql
+IF %ERRORLEVEL% NEQ 0 ECHO Error executing SQL file
